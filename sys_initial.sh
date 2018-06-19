2b60f4bc0b24bc26310f992e9d982205952983c1
Tiger <zhuhongjie0318@163.com>
2018-06-19 15:04:18 +0800

    add sys_initial.sh

---
+++ sys_initial.sh
@@ -0,0 +1,33 @@
+#node
+sudo apt-get install curl
+sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - 
+sudo apt-get install -y nodejs
+sudo apt-get install nodejs
+
+#redis
+sudo apt-get update
+sudo apt-get install redis-server
+
+#mysql
+mkdir ~/download
+cd ~/download
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.10-1_all.deb #设置mysql版本
+sudo apt-get update
+sudo apt-get install mysql-server
+
+#mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+#google输入法
+sudo apt-get install fcitx fcitx-googlepinyin im-config  
+im-config 
+
+#git
+sudo add-apt-repository ppa:git-core/ppa 
+sudo apt update
+sudo apt install git
+
